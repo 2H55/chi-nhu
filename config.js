1540b344781c2915988ddaa1bba33a03dced749b
2H55 <86763345+2H55@users.noreply.github.com>
2021-07-01 17:54:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,5 +14,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Khi nào về dẫn đi chơi nhó :)).',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'M.me/vinhphu14062004'
+    messLink: 'M.me/vinhphu14062004'//link mes của các bạn.
 }
